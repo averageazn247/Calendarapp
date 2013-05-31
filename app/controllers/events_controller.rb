f0b1ea74fb6deb6a64b056b12e011df481e9f155
khoa bui <averageazn1337@gmail.com>
2013-05-31 11:34:56 -0500

    running

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,12 @@
 class EventsController < ApplicationController
   def new
     @event = Event.new
-    @remind = Remind.new
+
   end
   
   def create
     @event = Event.new(params[:event])
-    @remind= Remind.new(params[:remind])
+
     
     if @event.save
    
