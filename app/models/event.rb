cd6859f229daa706dee46554e4d0e6094bd5e4c4
khoa bui <averageazn1337@gmail.com>
2013-05-31 15:13:31 -0500

    added check boxes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :dayof, :description, :name, :creator, :remind_attributes
+  attr_accessible :dayof, :description, :name, :creator, :remind_attributes, :monday, :tuesday, :wedsday, :thursday, :friday, :satday, :sunday
   belongs_to :user
 
 
