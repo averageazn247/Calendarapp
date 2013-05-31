460482d05917956a805104741781c90e01ab2c93
khoa bui <averageazn1337@gmail.com>
2013-05-31 15:01:09 -0500

    preremake

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,13 @@
 class EventsController < ApplicationController
   def new
     @event = Event.new
+   
 
   end
   
   def create
     @event = Event.new(params[:event])
-
+ 
     
     if @event.save
    
