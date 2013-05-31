4028ec55f0d412aecbd43bd0bc88a8c89eb97103
khoa bui <averageazn1337@gmail.com>
2013-05-31 10:35:46 -0500

    added emails to db

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ActiveRecord::Base
   attr_accessible :dayof, :description, :name, :user
+  belongs_to :user
+  def import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      Event.create! row.to_hash
+    end
+  end
 
 end
