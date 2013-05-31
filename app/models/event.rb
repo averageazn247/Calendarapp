50ce1314b0677e01e70a3f60929959c0962b8356
khoa bui <averageazn1337@gmail.com>
2013-05-31 11:53:06 -0500

    running verion

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
-  attr_accessible :dayof, :description, :name, :user
-   
+  attr_accessible :dayof, :description, :name, :creator
+  belongs_to :user
   def import(file)
     CSV.foreach(file.path, headers: true) do |row|
       Event.create! row.to_hash
