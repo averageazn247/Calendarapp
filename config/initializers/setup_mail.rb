d5d902e289798026d6730f35a25032204f854f49
khoa bui <averageazn1337@gmail.com>
2013-06-03 16:58:35 -0500

    working emailing

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "averageazn1337@gmail.com",
-  :password             => "ikilledyou",
+  :user_name            => "neuInternChristian@gmail.com",
+  :password             => "one343NEU",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
