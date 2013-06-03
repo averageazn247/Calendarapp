2bfe18c3249d823b8e4576f78099b947050ead76
Christian Bullock <cbullock@neuone.com>
2013-06-03 15:06:54 -0500

    more config stuff

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "neuInternChristian@gmail.com"
+  default from: "www.neuInternChristian@gmail.com"
   
   def welcome_email(user)
     @user = user
