63dc53a9f5cda456346de4257c34d60648341308
khoa bui <averageazn1337@gmail.com>
2013-06-05 16:04:04 -0500

    jkljl

--- db/migrate/20130605183451_add_id_to_users.rb
+++ db/migrate/20130605183451_add_id_to_users.rb
@@ -1,5 +1,5 @@
 class AddIdToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :userid, :integer
+    add_column :users, :userid, :integer, :unique => true
   end
 end
--- db/migrate/20130605183742_add_id_to_events.rb
+++ db/migrate/20130605183742_add_id_to_events.rb
@@ -1,6 +1,6 @@
 class AddIdToEvents < ActiveRecord::Migration
   def change
   add_column :events, :userid, :integer
-  add_column :events, :eventid, :integer
+  add_column :events, :eventid, :integer, :unique => true
   end
 end
