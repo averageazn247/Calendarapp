c85551418172e41448ba8839be1ca597ac5883ae
Christian Bullock <cbullock@neuone.com>
2013-06-06 09:38:45 -0500

    added holiday new view

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -1,9 +1,18 @@
 class HolidaysController < ApplicationController
   def new
-    
+    @holiday = Holiday.new
   end
   
   def create
+    @user = current_user
+    @holiday = Holiday.new(params[:holiday]) 
     
+    if @holiday.save
+   
+      flash[:success] = "Your Holiday has been added. You will not recieve notifications on this day."
+      redirect_to @user
+    else
+      render 'new'
+    end 
   end
 end
